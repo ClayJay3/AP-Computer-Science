fc7baa6502d0ae2df73dcaa31d7c8a27720d6332
root <root@kali>
2018-10-04 13:15:28 -0500

    Create Four.Seven

---
+++ Chapter 4/src/Four/Seven/Book.java
@@ -0,0 +1,17 @@
+package Four.Seven;
+
+public class Book
+{
+	private String title;
+	private String author;
+	private String publisher;
+	private int date;
+	
+	public Book(String title, String author, String publisher, int date)
+	{
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.date = date;
+	}
+}
