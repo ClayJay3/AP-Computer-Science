4826a5cdb5c6daa5395d47704194e92353f19c1a
root <root@kali>
2018-10-05 09:10:50 -0500

    Start Four.Eight Package

---
+++ Chapter 4/src/Four/Eight/Flight.java
@@ -0,0 +1,61 @@
+package Four.Eight;
+
+public class Flight
+{
+	// Create Instance Variables.
+	private String flightName;
+	private int flightNumber;
+	private String origin;
+	private String destination;
+	
+	// Create constructor.
+	public Flight(String flightName, int flightNumber, String origin, String destination)
+	{
+		this.flightName = flightName;
+		this.flightNumber = flightNumber;
+		this.origin = origin;
+		this.destination = destination;
+	}
+	
+	// Method for setters.
+	public void setFlightName(String flightName)
+	{
+		this.flightName = flightName;
+	}
+	public void setFlightNumber(int flightNumber)
+	{
+		this.flightNumber = flightNumber;
+	}
+	public void setOrigin(String origin)
+	{
+		this.origin = origin;
+	}
+	public void setDestination(String destination)
+	{
+		this.destination = destination;
+	}
+	
+	// Method for getters.
+	public String getFlightName()
+	{
+		return flightName;
+	}
+	public int getFlightNumber()
+	{
+		return flightNumber;
+	}
+	public String getOrigin()
+	{
+		return origin;
+	}
+	public String getDestination()
+	{
+		return destination;
+	}
+	
+	// Create toString.
+	public String toString()
+	{
+		return "Flight: " + flightName + " " + flightNumber + "";
+	}
+}
---
+++ Chapter 4/src/Four/Eight/FlightTest.java
@@ -0,0 +1,9 @@
+package Four.Eight;
+
+public class FlightTest
+{
+	public static void main(String[] args)
+	{
+		Flight plane1 = new Flight("American Airlines", "243", "Camdenton, ")
+	}
+}
