b9aeaee5d2dfc30a0912b9b8444dc1b008926b2b
root <root@kali>
2018-10-18 15:49:02 -0500

    Chapter 4 Project

--- Chapter 4/src/Chapter_4_Project/CreateLines.java
+++ Chapter 4/src/Chapter_4_Project/CreateLines.java
@@ -11,12 +11,15 @@ public class CreateLines
 		Scanner userInput = new Scanner(System.in);
 		Point point1 = new Point(0, 0);
 		Point point2 = new Point(0, 0);
+		Point point3 = new Point(0, 0);
 		
 		// Create global variables.
 		int point1x = 0;
 		int point1y = 0;
 		int point2x = 0;
 		int point2y = 0;
+		int point3x = 0;
+		int point3y = 0;
 		
 		// Ask user for both point locations.
 		System.out.println("Enter the x and y of the first point below.");
@@ -27,8 +30,29 @@ public class CreateLines
 		point2x = userInput.nextInt();
 		point2y = userInput.nextInt();
 		point2.setLocation(point2x, point2y);
+		System.out.println("Enter the x and y of the third point below.");
+		point3x = userInput.nextInt();
+		point3y = userInput.nextInt();
+		point3.setLocation(point3x, point3y);
 		
 		// Create a line with the previous point locations.
 		Line line = new Line(point1, point2);
+		
+		// Manipulate Line object for fun???
+		System.out.println("");
+		System.out.println("----------<Calculations with Given Info>-----------");
+		System.out.println("Distance between given points: " + line.calculateLineDistance());
+		System.out.println("Slope of given line: " + line.calculateSlope());
+		System.out.println("Y intercept of given line: " + line.calculateYIntercept());
+		System.out.println("Final equation of given line: " + line.getEquation());
+		System.out.println("---------------------------------------------------");
+		System.out.println("-----------------<Other Calculations>--------------");
+		System.out.println("Equation of a line perpendicular to the first with a given point: " + line.getPerpendicular(point3));
+		System.out.println("Equation of a line parallel to the first with a given point: " + line.getParallel(point3));
+		System.out.println("Radius of the tangent circle from a point to a line: " + line.calculatePointDistance(point3));
+		System.out.println("Equation of a circle tangent to given line with a center at point3: " + line.getTangentCircle(point3));
+		System.out.println("---------------------------------------------------");
+		System.out.println();
+		System.out.println("toString output: " + line.getEquation());
 	}
 }
--- Chapter 4/src/Chapter_4_Project/Line.java
+++ Chapter 4/src/Chapter_4_Project/Line.java
@@ -7,6 +7,16 @@ public class Line
 	// Create instance variables.
 	private Point point1;
 	private Point point2;
+	private Point point3;
+	private double lineDistance;
+	private double pointDistance;
+	private double lineSlope;
+	private double standardSlope;
+	private double perpendicularSlope;
+	private double lineYIntercept;
+	private double standardYIntercept;
+	private double perpendicularYIntercept;
+	private double parallelYIntercept;
 	
 	// Object constructor.
 	public Line(Point point1, Point point2)
@@ -16,9 +26,94 @@ public class Line
 	}
 	
 	// Calculate the distance between the given points.
-	public int calculateLineDistance()
+	public double calculateLineDistance()
 	{
-		int distance;
-		distance = Math.sqrt()
+		lineDistance = Math.sqrt(Math.pow((point2.x - point1.x), 2.0) + (Math.pow((point2.y - point1.y), 2.0)));
+		return lineDistance;
+	}
+	
+	// Calculate distance of a point from the line.
+	public double calculatePointDistance(Point point3)
+	{
+		this.point3 = point3;
+		standardSlope = lineSlope * -1;
+		standardYIntercept = lineYIntercept * -1;
+		pointDistance = Math.abs(((standardSlope * point3.x) + point3.y + standardYIntercept) / Math.sqrt(Math.pow(standardSlope, 2) + 1));
+		return pointDistance;
+	}
+	
+	// Calculate the slope of the given line.
+	public double calculateSlope()
+	{
+		lineSlope = (double) (point2.y - point1.y) / (point2.x - point1.x);
+		return lineSlope;
+	}
+	
+	// Calculate the Y Intercept of the given line.
+	public double calculateYIntercept()
+	{
+		lineYIntercept = point1.y - (lineSlope * point1.x);
+		return lineYIntercept;
+	}
+	
+	// Calculate a line that is perpendicular to the original line based on a given point.
+	public String getPerpendicular(Point point3)
+	{
+		this.point3 = point3;
+		perpendicularSlope = Math.pow(lineSlope, -1) * -1;
+		perpendicularYIntercept = point3.y - (perpendicularSlope * point3.x);
+		return "y=" + perpendicularSlope + "x" + " + " + perpendicularYIntercept;
+	}
+	
+	// Calculate a line that is parallel to the original line based on a given point.
+	public String getParallel(Point point3)
+	{
+		this.point3 = point3;
+		parallelYIntercept = point3.y - (lineSlope * point3.x);
+		return "y=" + lineSlope + "x" + " + " + parallelYIntercept;
+	}
+	
+	// Calculate and place a circle depending on the given line and the third point.
+	public String getTangentCircle(Point point3)
+	{
+		double radius;
+		this.point3 = point3;
+		
+		radius = calculatePointDistance(point3);
+		return "(x-" + point3.x + ")^2 + (y-" + point3.y + ")^2 = " + Math.pow(radius, 2);
+	}
+	
+	// Setters that set point values.
+	public void setPoint1(Point point1)
+	{
+		this.point1 = point1;
+	}
+	public void setPoint2(Point point2)
+	{
+		this.point2 = point2;
+	}
+	public void setPoint3(Point point3)
+	{
+		this.point3 = point3;
+	}
+	
+	// Getters that returns point values.
+	public Point getPoint1()
+	{
+		return point1;
+	}
+	public Point getPoint2()
+	{
+		return point2;
+	}
+	public Point getPoint3()
+	{
+		return point3;
+	}
+	
+	// Getter that returns a string that represents the equation of the line y=mx+b.
+	public String getEquation()
+	{
+		return "y=" + lineSlope + "x" + " + " + lineYIntercept;
 	}
 }
