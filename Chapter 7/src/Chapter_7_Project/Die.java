6d1321e88936d7dca503d30a44269de74a4aa766
root <root@kali>
2019-01-10 13:17:24 -0600

    Chapter 7 Programming Problems

--- Chapter 7/src/Chapter_7_Project/Die.java
+++ Chapter 7/src/Chapter_7_Project/Die.java
@@ -5,26 +5,46 @@ public class Die
 	private final int MAX = 6;
 	private int faceValue;
 	private int id;
+	private boolean locked;
 	
 	// Create Constructor
 	public Die(int id)
 	{
 		this.id = id;
+		locked = false;
 		roll();
 	}
 	
 	// Roll dice
 	public int roll()
 	{
-		faceValue = (int) (Math.random() * MAX) + 1;
-		return faceValue;
+		if (!locked)
+		{
+			faceValue = (int) (Math.random() * MAX) + 1;
+			return faceValue;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	// Reset the dice so its locked.
+	public void lock(boolean locked)
+	{
+		this.locked = locked;
+		faceValue = 0;
 	}
 	
 	// Create setters.
 	public void setFaceValue(int value)
 	{
-		faceValue = value;
+		if (!locked)
+		{
+			faceValue = value;
+		}
 	}
+	
 	
 	// Create getters
 	public int getFaceValue()
--- Chapter 7/src/Chapter_7_Project/Yahtzee.java
+++ Chapter 7/src/Chapter_7_Project/Yahtzee.java
@@ -10,8 +10,8 @@ public class Yahtzee
 		// Create Objects and Variables.
 		ArrayList<Die> cupOfDice = new ArrayList<Die>();
 		ArrayList<ArrayList> dicePairs = new ArrayList<ArrayList>();
-		int gameCounter = 0;
-		int indexStepper = 1;
+		int gameCounter = 1;
+		int roundCounter = 0;
 		int largestPair = 0;
 		int numberOfMatchingDice = 0;
 		int pointsThisRound = 0;
@@ -27,54 +27,98 @@ public class Yahtzee
 		cupOfDice.add(die4);
 		cupOfDice.add(die5);
 		boolean stop = false;
+		boolean keepPlaying = true;
 		Scanner userInput = new Scanner(System.in);
 
-		// Start the game.
-		System.out.println("Hit enter to roll the 5 dice.");
-		userInput.nextLine();
-		for (Die die : cupOfDice) { die.roll(); System.out.println("Die" + die.getID() + ": " + die.getFaceValue()); }
-		
-		// Determine the matching pairs.
-		for (Die die : cupOfDice)
+		// Keep playing until game ends or user stops.
+		while (keepPlaying && gameCounter < 5)
 		{
-			ArrayList<Integer> pairs = new ArrayList<Integer>();
-			for (Die otherDie : cupOfDice)
+			do
 			{
-				if (die.getID() < otherDie.getID())
+				// Start the game.
+				System.out.println("Hit enter to roll the 5 dice.");
+				userInput.nextLine();
+				for (Die die : cupOfDice) { die.roll(); System.out.println("Die" + die.getID() + ": " + die.getFaceValue()); }
+				
+				// Determine the matching pairs.
+				for (Die die : cupOfDice)
 				{
-					if (die.getFaceValue() == otherDie.getFaceValue())
+					if (die.getFaceValue() != 0)
 					{
-						while (!stop)
+						ArrayList<Integer> pairs = new ArrayList<Integer>();
+						for (Die otherDie : cupOfDice)
 						{
-							pairs.add(die.getFaceValue());
-							stop = true;
+							if (otherDie.getFaceValue() != 0)
+							{
+								if (die.getID() < otherDie.getID())
+								{
+									if (die.getFaceValue() == otherDie.getFaceValue())
+									{
+										while (!stop)
+										{
+											pairs.add(die.getFaceValue());
+											stop = true;
+										}
+										pairs.add(otherDie.getFaceValue());
+									}
+								}
+							}
 						}
-						pairs.add(otherDie.getFaceValue());
+						dicePairs.add(pairs);
+						stop = false;
 					}
 				}
-			}
-			dicePairs.add(pairs);
-			stop = false;
-		}
-		
-		// Print the dice pairs and keep to largest pair.
-		System.out.println();
-		System.out.println("Dice pairs found by the program:");
-		for (ArrayList pairArray : dicePairs)
-		{
-			for (Object number : pairArray)
-			{
-				if (Integer.parseInt(number.toString()) > largestPair)
+				
+				// Print the dice pairs and keep to largest pair.
+				System.out.println();
+				System.out.println("Dice pairs found by the program:");
+				for (ArrayList pairArray : dicePairs)
 				{
-					largestPair = Integer.parseInt(number.toString());
-					System.out.println(pairArray);
-					numberOfMatchingDice = pairArray.size();
+					for (Object number : pairArray)
+					{
+						if (Integer.parseInt(number.toString()) > largestPair)
+						{
+							largestPair = Integer.parseInt(number.toString());
+							System.out.println(pairArray);
+							numberOfMatchingDice = pairArray.size();
+							for (int i = 0; i < numberOfMatchingDice; i++)
+							{
+								cupOfDice.get(i).lock(true);
+							}
+						}
+					}
 				}
-			}
+				
+				// Add points and increment counters.
+				pointsThisRound += largestPair * numberOfMatchingDice;
+				roundCounter ++;
+				
+				// Print out user info.
+				System.out.println("Largest Dice Pair Value: " + largestPair);
+				System.out.println("Number Of Matching Die: " + numberOfMatchingDice);
+				System.out.println("Total Number of Points Earned this Round: " + pointsThisRound);
+				System.out.println();
+				
+				// Reset values for next round.
+				largestPair = 0;
+				numberOfMatchingDice = 0;
+				dicePairs.clear();
+			} while (roundCounter < 2);
+			
+			// Add points to total game score and reset stuff.
+			totalScore =+ pointsThisRound;
+			pointsThisRound = 0;
+			roundCounter = 0;
+			for (Die die : cupOfDice) { die.lock(false); }
+			
+			// Ask the user if they want to keep playing.
+			System.out.println();
+			System.out.println();
+			System.out.println("Round Number: " + gameCounter);
+			System.out.println("Total Score for the Game: " + totalScore);
+			System.out.println("Do you want to keep playing? (y/n)");
+			keepPlaying = (userInput.nextLine().equalsIgnoreCase("y")) ? true : false;
+			
 		}
-		pointsThisRound = largestPair * numberOfMatchingDice;
-		System.out.println("Largest Dice Pair Value: " + largestPair);
-		System.out.println("Number Of Matching Die: " + numberOfMatchingDice);
-		System.out.println("Total Number of Points Earned this Round: " + pointsThisRound);
 	}
 }
