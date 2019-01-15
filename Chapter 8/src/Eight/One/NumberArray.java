93b5c81225e926986dc84dda036b5b0b9b8341bc
root <root@kali>
2019-01-15 12:44:59 -0600

    Chapter 8 Programming Problems

---
+++ Chapter 8/src/Eight/One/NumberArray.java
@@ -0,0 +1,15 @@
+package Eight.One;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class NumberArray
+{
+	public static void main(String[] args)
+	{
+		// Create objects, variables, and arrays.
+		int[] numberTally = new int[51];
+		Scanner fileInput = new Scanner(new File(""));
+		
+	}
+}
