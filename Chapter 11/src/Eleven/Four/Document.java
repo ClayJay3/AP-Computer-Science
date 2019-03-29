d5a37f8c320d3719b8ab927710c70d83e4656a7e
root <root@kali>
2019-03-29 11:56:23 -0500

    Chapter 11 Programming Problems

---
+++ Chapter 11/src/Eleven/Four/Document.java
@@ -0,0 +1,64 @@
+package Eleven.Four;
+
+public class Document
+{
+	// Create objects and variables.
+	private String name, date, author;
+	private int pages;
+	private char documentCode = 'U', identifier;
+	private InvalidDocumentCodeException problem = new InvalidDocumentCodeException("ERROR: Invalid document code!");
+	
+	// Object Constructor.
+	public Document(String name, String date, String author, int pages, char documentCode, char identifier) throws InvalidDocumentCodeException
+	{
+		this.name = name;
+		this.date = date;
+		this.author = author;
+		this.pages = pages;
+		setDocumentCode(documentCode);	// Catch any possible exceptions inside the setter.
+		this.identifier = identifier;
+	}
+	
+	// GETTERS (mixing it up a bit)
+	public String getName() { return name; }
+	public String getDate() { return date; }
+	public String getAuthor() { return author; }
+	public int getPages() { return pages; }
+	public char getDocumentCode() { return documentCode; }
+	public char getIdentifier() { return identifier; }
+	
+	// SETTERS (mixing it up a bit)
+	public void setName(String name) { this.name = name; }
+	public void setDate(String date) { this.date = date; }
+	public void setAuthor(String author) { this.author = author; }
+	public void setPages(int pages) { this.pages = pages; }
+	public void setDocumentCode(char documentCode) throws InvalidDocumentCodeException 
+	{ 
+		// Determine if the documentCode is invalid.
+		try
+		{
+			if (documentCode != 'U' && documentCode != 'C' && documentCode != 'P')
+			{
+				throw problem;
+			}
+			else
+			{
+				this.documentCode = documentCode;
+			}
+		}
+		catch (InvalidDocumentCodeException e)
+		{
+			problem.printStackTrace();
+			System.out.println();
+			System.out.println("The program will defualt to \"Unclassified\"....");
+			System.out.println();
+		}
+	} 
+	public void setIdentifier(char identifier) { this.identifier = identifier; }
+	
+	// Objects toString.
+	public String toString()
+	{
+		return getName() + "(" + getDocumentCode() + getIdentifier() + ")" + "\nAuthor: " + getAuthor() + "\nDate: " + getDate() + "\nPages: " + getPages() + "\n";
+	}
+}
---
+++ Chapter 11/src/Eleven/Four/InvalidDocumentCodeException.java
@@ -0,0 +1,9 @@
+package Eleven.Four;
+
+public class InvalidDocumentCodeException extends Exception
+{
+	public InvalidDocumentCodeException(String message)
+	{
+		super(message);
+	}
+}
